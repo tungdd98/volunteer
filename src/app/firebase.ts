ec5f530e5d7c6f07e71fdecd89fceab359287bd6
tungdd <tungdd98@gmail.com>
2022-06-05 03:17:39 +0700

    update env

--- src/app/firebase.ts
+++ src/app/firebase.ts
@@ -7,13 +7,13 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAcgDjittcp75s84KuCupMCJ8TBM70pGno",
-  authDomain: "volunteer-305007.firebaseapp.com",
-  projectId: "volunteer-305007",
-  storageBucket: "volunteer-305007.appspot.com",
-  messagingSenderId: "637986262445",
-  appId: "1:637986262445:web:9325c5619b98c18efbe524",
-  measurementId: "G-58Z79MQ4M5",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
