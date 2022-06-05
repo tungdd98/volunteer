b86dc917223bf2a0cd25070d2c3aad4c22e0a761
tungdd <tungdd98@gmail.com>
2022-06-05 17:49:16 +0700

    update detail

--- src/features/article/screens/ArticleDetailScreen/ArticleDetailScreen.tsx
+++ src/features/article/screens/ArticleDetailScreen/ArticleDetailScreen.tsx
@@ -4,6 +4,7 @@ import React, { FC, useEffect, useState } from "react";
 import { ShareRounded } from "@mui/icons-material";
 import {
   Box,
+  Button,
   Chip,
   Container,
   IconButton,
@@ -45,81 +46,107 @@ const ArticleDetailScreen: FC = () => {
   }
 
   return (
-    <Container sx={{ mt: 3 }}>
-      <PreviewImage
-        src={articleDetail.thumbnail}
-        alt={articleDetail.title}
-        aspectRatio={AspectRatioEnum.SIXTEEN_TO_NINE}
-      />
-
-      <Typography variant="h1" fontSize={24} sx={{ mt: 1, mb: 2 }}>
-        {articleDetail.title}
-      </Typography>
-
-      <Stack spacing={1} direction="row" sx={{ width: "100%" }}>
-        {articleDetail.tags.map(item => (
-          <Chip key={item} label={item} color="info" size="small" />
-        ))}
-
-        <Box sx={{ ml: "auto !important" }}>
-          <IconButton size="small">
-            <ShareRounded />
-          </IconButton>
-        </Box>
-      </Stack>
-
-      <Box sx={{ flexGrow: 1, my: 2 }}>
-        <Typography variant="body2" fontWeight={600}>
-          {toCurrency(articleDetail.currentDonate, true)} /{" "}
-          {toCurrency(articleDetail.maxDonate, true)} Lượt
-        </Typography>
-        <BorderLinearProgress
-          variant="determinate"
-          value={Math.floor(
-            (articleDetail.currentDonate / articleDetail.maxDonate) * 100
-          )}
-          sx={{ my: 0.5 }}
+    <>
+      <Container sx={{ mt: 3, mb: 10 }}>
+        <PreviewImage
+          src={articleDetail.thumbnail}
+          alt={articleDetail.title}
+          aspectRatio={AspectRatioEnum.SIXTEEN_TO_NINE}
         />
-        <Typography variant="body2">
-          {toCurrency(articleDetail.currentDonate, true)} người ủng hộ
-        </Typography>
-      </Box>
 
-      <Box sx={{ mb: 5 }}>
-        <Typography variant="subtitle1" gutterBottom>
-          Hoàn cảnh
+        <Typography variant="h1" fontSize={24} sx={{ mt: 1, mb: 2 }}>
+          {articleDetail.title}
         </Typography>
-        <Typography variant="body2">{articleDetail.content}</Typography>
-      </Box>
 
-      <CustomSwiper
-        slidesPerView="auto"
-        pagination={{
-          dynamicBullets: true,
-        }}
-        loop
-        autoHeight
-        breakpoints={{
-          "640": {
-            slidesPerView: 2,
-            spaceBetween: 10,
-          },
-          "1024": {
-            slidesPerView: 3,
-            spaceBetween: 10,
-          },
+        <Stack spacing={1} direction="row" sx={{ width: "100%" }}>
+          {articleDetail.tags.map(item => (
+            <Chip key={item} label={item} color="info" size="small" />
+          ))}
+
+          <Box sx={{ ml: "auto !important" }}>
+            <IconButton size="small">
+              <ShareRounded />
+            </IconButton>
+          </Box>
+        </Stack>
+
+        <Box sx={{ flexGrow: 1, my: 2 }}>
+          <Typography variant="body2" fontWeight={600}>
+            {toCurrency(articleDetail.currentDonate, true)} /{" "}
+            {toCurrency(articleDetail.maxDonate, true)} Lượt
+          </Typography>
+          <BorderLinearProgress
+            variant="determinate"
+            value={Math.floor(
+              (articleDetail.currentDonate / articleDetail.maxDonate) * 100
+            )}
+            sx={{ my: 0.5 }}
+          />
+          <Typography variant="body2">
+            {toCurrency(articleDetail.currentDonate, true)} người ủng hộ
+          </Typography>
+        </Box>
+
+        <Box sx={{ mb: 5 }}>
+          <Typography variant="subtitle1" gutterBottom>
+            Hoàn cảnh
+          </Typography>
+          <Typography variant="body2">{articleDetail.content}</Typography>
+        </Box>
+
+        <CustomSwiper
+          slidesPerView="auto"
+          pagination={{
+            dynamicBullets: true,
+          }}
+          loop
+          autoHeight
+          breakpoints={{
+            "640": {
+              slidesPerView: 2,
+              spaceBetween: 10,
+            },
+            "1024": {
+              slidesPerView: 3,
+              spaceBetween: 10,
+            },
+          }}
+        >
+          {articleDetail.albums.map(item => (
+            <SwiperSlide key={item}>
+              <PreviewImage
+                src={item}
+                aspectRatio={AspectRatioEnum.SIXTEEN_TO_NINE}
+              />
+            </SwiperSlide>
+          ))}
+        </CustomSwiper>
+      </Container>
+
+      <Box
+        sx={{
+          position: "fixed",
+          width: "100%",
+          bottom: 0,
+          left: 0,
+          p: 2,
+          bgcolor: "white",
+          borderTop: 1,
+          borderColor: "divider",
+          zIndex: 100,
+          textAlign: "center",
         }}
       >
-        {articleDetail.albums.map(item => (
-          <SwiperSlide key={item}>
-            <PreviewImage
-              src={item}
-              aspectRatio={AspectRatioEnum.SIXTEEN_TO_NINE}
-            />
-          </SwiperSlide>
-        ))}
-      </CustomSwiper>
-    </Container>
+        <Button
+          variant="contained"
+          fullWidth
+          size="large"
+          sx={{ maxWidth: 500 }}
+        >
+          Ủng hộ ngay
+        </Button>
+      </Box>
+    </>
   );
 };
 
